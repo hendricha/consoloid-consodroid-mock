15bcb9e3921f89b25699aeea8e935f80ee1b7f4e
hendricha <hendricha@gmail.com>
2014-10-08 12:55:44 +0200

    Added run.js
    this starts a http server
    atempts to require contextify and fs-ext
    and also writes an accesControl file after some timeout

---
+++ run.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs-ext');
+var contextify = require('contextify');
+
+var message = "Hello Consodroid\n";
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end(message);
+});
+server.listen(3000);
+
+setTimeout(function() {
+  fs.writeFileSync("/data/data/hu.hendricha.consodroid/files/accessControl/10.0.0.4", "");
+  message += "Wrote file";
+}, 5000);
